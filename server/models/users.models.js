534fe67e9c552936fec2032d7fb1aa6535b91e97
EileenJuergens <juergens.eileen@gmail.com>
2019-10-05 18:43:56 +0200

    feat(user.models): define schema

---
+++ server/models/users.models.js
@@ -0,0 +1,27 @@
+const mongoose = require('../db');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  coordinates: {
+    lat: {type: String},
+    long:{type: String},
+  },
+  phoneNumber: {type: String},
+  email: {type: String},
+  hashPassword: {type: String},
+  google: {type: String},
+  numOfBols: {type: Number},
+  pendingTransactions: {type: Array},
+  previousTransactions: {type: Array},
+  notifications: {
+    firstNoti: {type: Boolean},
+    secondNoti: {type: Boolean},
+    lastNoti: {type: Boolean},
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
