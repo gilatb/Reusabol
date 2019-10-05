2259fdf89890e00af0bfc2632d7dcf004c0425c3
lmandersson <linnea.m.andersson@gmail.com>
2019-10-05 19:24:40 +0200

    feat(structure): basic structure RestoModal

--- client/src/components/RestoModal.js
+++ client/src/components/RestoModal.js
@@ -5,6 +5,8 @@ import Image from './atomic-components/Image';
 import Title from './atomic-components/Title';
 import Subtitle from './atomic-components/Subtitle';
 import SquareBtn from '../components/atomic-components/SquareBtn';
+import RoundBtn from './atomic-components/RoundBtn';
+import Counter from './atomic-components/Counter';
 
 export default function RestoModal () {
 
@@ -13,13 +15,15 @@ export default function RestoModal () {
 
   return (
     <div>
-      <Image alt={'User picture'} />
+      <Image alt={'User image'} />
       <Title text={'Order by ' + name + '!'} />
       <Subtitle text={'Please update the number of bowls and click confirm.'} />
-
+      <RoundBtn text={'+'} />
+      <Counter />
+      <Image alt={'Bowl image'} />
+      <RoundBtn text={'-'} />
       <SquareBtn text={'CONFIRM'} />
       <SquareBtn text={'CANCEL'} />
-
     </div>
       )
     }
