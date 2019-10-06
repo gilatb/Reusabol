689c1207ab6052cea7a3699b31567b29399115ca
EileenJuergens <juergens.eileen@gmail.com>
2019-10-06 18:47:16 +0200

    feat(restaurant): define resto controller

---
+++ server/controllers/restaurants.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Restaurant = require('../models/restaurants.models');
+
+exports.getRestaurants = async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find();
+    res.status(201);
+    res.json(restaurants);
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  } 
+};
+exports.createPlace = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.create(req.body);
+    res.status(201);
+    res.json(restaurant);
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
+}
