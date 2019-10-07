48708016657e77ce2ce55db18d0bef2327c3f8e6
lmandersson <linnea.m.andersson@gmail.com>
2019-10-07 19:31:13 +0200

    feat(redux): update user action

--- client/src/redux/actions/user.js
+++ client/src/redux/actions/user.js
@@ -1,7 +1,10 @@
-export function getUserData (userId, key) {
-  return {
-    type: 'GET_USER_DATA',
-    userId,
-  }
-}
+import ApiClient from '../../services/ApiClient';
 
+export const getUserData = userId => dispatch => {
+  ApiClient.getUserData(userId)
+  .then(userData => {
+    //TODO: CHANGE THE DISPATCH TO JUST SET THE USER FIRSTNAME AND SET USER NUMBOLS IN REDUX
+    // dispatch({type: 'SET_MOVIES', moviesList });
+    // dispatch({type: 'SET_LISTS', listName: category, list: moviesList});
+  })
+}
