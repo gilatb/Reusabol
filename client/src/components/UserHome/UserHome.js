098fb1d03f165168c4a4cdbccb654091efd2766a
lmandersson <linnea.m.andersson@gmail.com>
2019-10-07 12:19:42 +0200

    fix(structure): update dependencies to reflect name change from landpage to home

--- client/src/components/UserHome/UserHome.js
+++ client/src/components/UserHome/UserHome.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './UserHome.css';
 import Header from '../Header/Header';
 import Map from '../Map/Map'
+
 export default function UserHome () {
   return (
     <div className="user-home">
