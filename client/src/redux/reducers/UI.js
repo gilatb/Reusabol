d479a3ea5c4c3ab8fdaacef11b2bd1eb1e10888d
lmandersson <linnea.m.andersson@gmail.com>
2019-10-07 12:33:27 +0200

    feat(redux): add switch with default to reducers

--- client/src/redux/reducers/UI.js
+++ client/src/redux/reducers/UI.js
@@ -1,3 +1,10 @@
-export default function UI () {
-  //code
+const INITIAL_STATE = {
+
 };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
