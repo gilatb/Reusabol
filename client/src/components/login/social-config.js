852f065df8fff6458bf2cf0937e3d193234a5b3c
Andre DiFelice <andre.difelice@gmai.com>
2019-10-07 10:57:52 +0200

    add facebook and google login buttons

---
+++ client/src/components/login/social-config.js
@@ -0,0 +1,16 @@
+const facebook = {
+  appId: "",
+  cookie: true,
+  xfbml: true,
+  version: "v3.2",
+  scope: "email"
+};
+ 
+export const facebookConfig = facebook;
+
+const google = {
+  client_id: "",
+  scope: "profile email"
+};
+ 
+export const googleConfig = google;
