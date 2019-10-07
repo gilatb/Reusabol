41e2830bcfe631d482e7f7f372882ff1db8a77e9
Gilat <gilatblumberger@gmail.com>
2019-10-07 10:50:26 +0200

    feat(Map): add center state

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 
 import './Map.css';
@@ -7,6 +7,15 @@ import './Map.css';
 const API_KEY = process.env.REACT_APP_API_KEY
 
 export default function Map () {
+
+  const [center, setCenter] = useState({lat: 45.390205,lng: 2.154007});
+
+  const myPlaces = [
+    { id: "place1", pos: { lat: 39.09366509575983, lng: -94.58751660204751 } },
+    { id: "place2", pos: { lat: 39.10894664788252, lng: -94.57926449532226 } },
+    { id: "place3", pos: { lat: 39.07602397235644, lng: -94.5184089401211 } }
+  ];
+
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: API_KEY
   })
@@ -23,10 +32,7 @@ export default function Map () {
           height: "70vh",
           width: "100%"
         }}
-        center={{
-          lat: 45.390205,
-          lng: 2.154007
-        }}
+        center={center}
         zoom={10}
         onLoad={onMapLoad}
       >
