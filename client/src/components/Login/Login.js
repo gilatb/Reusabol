fbcc83d747cb6135ae087f1afd8829477c137588
lmandersson <linnea.m.andersson@gmail.com>
2019-10-08 20:42:31 +0200

    feat(sockets): test sockets

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import axios from 'axios';
 
 import SquareBtn from '../atomic-components/SquareBtn/SquareBtn';
@@ -15,6 +16,19 @@ export default function Login () {
 
   //Get rid of the onCLick 'me' button once done testing
   //need to import SquareBtn from atomic-componenets
+=======
+import { NavLink } from 'react-router-dom';
+import socketIOClient from 'socket.io-client';
+
+export default function Login () {
+
+  const socket = socketIOClient.connect('http://localhost:4000', {reconnect: true});
+
+  socket.on('test', (data) => {
+    console.log('data.message: ', data.message);
+  })
+
+>>>>>>> feat(sockets): test sockets
   return (
 
     <div>
