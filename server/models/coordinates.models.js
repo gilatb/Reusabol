2ba5214fd0e54430158993a720a8b08674c814fa
EileenJuergens <juergens.eileen@gmail.com>
2019-10-08 20:01:15 +0200

    feat(coordModel): define coordinates schema

---
+++ server/models/coordinates.models.js
@@ -0,0 +1,9 @@
+const mongoose = require('../db');
+const Schema = mongoose.Schema;
+
+const CoordinatesSchema = new Schema({
+  lat: Number,
+  lng: Number
+});
+
+module.exports = CoordinatesSchema;
