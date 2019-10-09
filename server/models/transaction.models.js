599ef1b0648ce31bf4f544ed26eb54f2dfc94141
EileenJuergens <juergens.eileen@gmail.com>
2019-10-09 18:33:43 +0200

    feat(transModel): add transId

--- server/models/transaction.models.js
+++ server/models/transaction.models.js
@@ -1,8 +1,8 @@
 const mongoose = require('../db');
 const Schema = mongoose.Schema;
 
-
 const TransactionSchema = new Schema({
+  transId: String,
   userId: String,
   restoId: String,
   numBols: {type:Number, default:0}
