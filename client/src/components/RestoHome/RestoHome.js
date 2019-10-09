6d9509ff87c65881c14889538f8d8e5f6f3f0399
lmandersson <linnea.m.andersson@gmail.com>
2019-10-09 18:48:11 +0200

    refactor(structure): move and rename modalcomp to restohistorymodal

--- client/src/components/RestoHome/RestoHome.js
+++ client/src/components/RestoHome/RestoHome.js
@@ -4,8 +4,7 @@ import './RestoHome.css';
 import Header from '../Header/Header';
 import List from '../List/List';
 import Footer from '../Footer/Footer';
-import ModalComp from '../atomic-components/Modal/ModalComp';
-import RestoHistoryModal from '../RestoHistoryModal/RestoHistoryModal';
+
 
 //TODO: DELETE SQUAREBTN ONCE THE MODAL WORKS AND YOU MOVE THIS FUNCTIONALITY TO THE BUTTON IN THE FOOTER
 import SquareBtn from '../atomic-components/SquareBtn/SquareBtn';
