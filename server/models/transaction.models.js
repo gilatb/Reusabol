bf883553334affd803f74d49f7d66fd25f21d0a4
EileenJuergens <juergens.eileen@gmail.com>
2019-10-09 13:06:59 +0200

    feat(transModel): remove transId & change numBols type

--- server/models/transaction.models.js
+++ server/models/transaction.models.js
@@ -1,11 +1,11 @@
 const mongoose = require('../db');
 const Schema = mongoose.Schema;
 
+
 const TransactionSchema = new Schema({
-  transId: String,
   userId: String,
   restoId: String,
-  numBols: Number
+  numBols: {type:Number, default:0}
 });
 
 module.exports = TransactionSchema;
