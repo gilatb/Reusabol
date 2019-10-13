f7eccb68c5e27db3a94d356c9b06e525a27a1e34
lmandersson <linnea.m.andersson@gmail.com>
2019-10-13 22:12:06 +0200

    refactor(redux): remove commented code

--- client/src/redux/reducers/user.js
+++ client/src/redux/reducers/user.js
@@ -2,6 +2,7 @@ const INITIAL_STATE = {
   userData: {
     firstName: '',
     lastName: '',
+    googleImage: '',
     numBols: 0,
   },
 };
@@ -15,6 +16,7 @@ export default (state = INITIAL_STATE, action) => {
           ...state.userData,
           firstName: action.name.firstName,
           lastName: action.name.lastName,
+          googleImage: action.name.googleImage,
         }
       };
 
