44b847cc64bb37c1fb54223f08c31e53cb1ab200
EileenJuergens <juergens.eileen@gmail.com>
2019-10-15 16:24:01 +0200

    fix(squareBtn): uncomment

--- client/src/components/atomic-components/SquareBtn/SquareBtn.js
+++ client/src/components/atomic-components/SquareBtn/SquareBtn.js
@@ -1,16 +1,16 @@
-// import React from 'react';
-// import { Button } from '@material-ui/core';
-// import './SquareBtn.css';
+import React from 'react';
+import { Button } from '@material-ui/core';
+import './SquareBtn.css';
 
-// export default function SquareBtn ({ text, onClick }) {
-//   return (
-//     <div>
-//       <Button
-//         variant="contained"
-//         size="small"
-//         children={text}
-//         onClick={onClick}
-//     />
-//     </div>
-//   )
-// }
+export default function SquareBtn ({ text, onClick }) {
+  return (
+    <div>
+      <Button
+        variant="contained"
+        size="small"
+        children={text}
+        onClick={onClick}
+    />
+    </div>
+  )
+}
