6b5b26d39cad00f6a9fa0849cdbc894ac71a1700
EileenJuergens <juergens.eileen@gmail.com>
2019-10-15 12:58:41 +0200

    feat(header): modify styling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 import './Header.css';
-import ImageComp from '../atomic-components/Image/ImageComp';
-import { IoMdPower } from 'react-icons/io'
+import Image from '../atomic-components/Image/Image';
+import { IoIosMenu } from 'react-icons/io'
 
 export default function Header() {
   return (
     <div className="header">
-      <ImageComp alt={'Logo image'} src={''} />
       <a href='http://localhost:4000/logout'>Logout</a>
-      {/* <ImageComp alt={'Burger image'} /> */}
-      <IoMdPower className="logout-btn"/>
+      <Image alt={'Logo image'} />
+      <IoIosMenu className="menu-btn"/>
     </div>
   )
 }
+
+
+
+
