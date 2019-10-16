e1a24d0c00938467973b5d51befa79cd769dccad
EileenJuergens <juergens.eileen@gmail.com>
2019-10-16 14:35:34 +0200

    refactor(list): split title

---
+++ client/src/components/atomic-components/Pretitle/Pretitle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Pretitle.css';
+
+export default function Pretitle ({ text }) {
+  return (
+    <div className="pretitle">
+      <span>{text}</span>
+    </div>
+  )
+}
